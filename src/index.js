77b6c071cec5c3ed0379e045a197326edfe8b663
Gopinath U <epulz2k24@gmail.com>
2024-02-18 16:32:28 +0530

    hashrouter

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
