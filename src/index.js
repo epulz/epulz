23351d7ae572c0d95242e56038be9e9fe8c390d3
Gopinath U <epulz2k24@gmail.com>
2024-02-18 16:48:46 +0530

    Finish

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+
   <React.StrictMode>
-    <HashRouter>
+
       <App />
-    </HashRouter>
+  
   </React.StrictMode>
+
 );
